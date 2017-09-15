3e12b5cb78db76996a536b45bda6f59762c5b37e
Sergey <sergey.kabak95@gmail.com>
2017-09-15 10:07:06 +0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require('gulp'),
-		sass = require('gulp-sass');
+		sass = require('gulp-sass'),
+		git = require('gulp-git');
 
 gulp.task('sass', function(){
 	return gulp.src('proj1/sass/**/*.sass')
 	.pipe(sass())
 	.pipe(gulp.dest('proj1/css/'))
 });
+gulp.task('add', function(){
+  return gulp.src('proj1/index.html')
+    .pipe(git.add());
+});
 
 gulp.task('watch', function(){
-	gulp.watch('proj1/sass/**/*.sass', function(event, cb) {
+	gulp.watch('proj1/sass/**/*.sass', ['add'], function(event, cb) {
 		setTimeout(function(){gulp.start('sass');},500) // задача выполниться через 500 миллисекунд и файл успеет сохраниться на диске
 	});
 });
